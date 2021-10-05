9a2aa0b57df747ee691eff62a9bf7729e7109df6
Filip <Filip@FILIP-DATOR>
2021-10-05 09:03:51 +0200

    Add project files.

---
+++ DemoForGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoForGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
